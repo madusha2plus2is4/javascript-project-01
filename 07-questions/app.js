cf1c705f1764a7f7e4830220e9f6d7358869b0c1
Madusha <madusha.20200573@iit.ac.lk>
2022-01-29 19:04:29 +0530

    completed project

---
+++ 07-questions/app.js
@@ -0,0 +1,19 @@
+//using selectors inside the element
+
+// const questions = document.querySelectorAll(".question");
+
+// questions.forEach((question) => {
+//   const btn = question.querySelector(".question-btn");
+//   btn.addEventListener("click", () => question.classList.toggle("show-text"));
+// });
+
+// traversing the dom
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
+  });
+});
